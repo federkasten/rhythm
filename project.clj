4a4c879574f5e8b93d114e9e0bbf6dd4b0990522
Takashi AOKI <federkasten@me.com>
2014-05-28 20:51:36 -0700

    cleanup project configuration

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject rhythm "0.1.0-SNAPSHOT"
-  :description "A minimal ClojureScript library handling states and events"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]]
+(defproject rhythm "0.1.1-SNAPSHOT"
+  :description "A ClojureScript library handling states and events"
   :plugins [[lein-cljsbuild "1.0.3"]]
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.4"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-2202"]]}}
   :source-paths ["src"]
   :cljsbuild {:builds
               {:dev
