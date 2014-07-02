dced9ec00a3c3f9799da2b59545716688751a929
Takashi AOKI <federkasten@me.com>
2014-07-02 15:55:36 +0900

    add license terms

--- README.md
+++ README.md
@@ -49,3 +49,12 @@ To remove watcher
 ```clj
 (trigger-action! example-actor :load)
 ```
+
+## License
+
+Copyright 2014 [Takashi AOKI][federkasten]
+
+Licensed under the [Apache License, Version 2.0][apache-license-2.0].
+
+[federkasten]: http://federkasten.net
+[apache-license-2.0]: http://www.apache.org/licenses/LICENSE-2.0.html
--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject rhythm "0.1.1-SNAPSHOT"
-  :description "A ClojureScript library handling states and events"
+  :description "A ClojureScript library handling states and actions for event-driven programming"
   :url "https://github.com/federkasten/rhythm"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :plugins [[lein-cljsbuild "1.0.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/clojurescript "0.0-2227"]]}}
+                                  [org.clojure/clojurescript "0.0-2261"]]}}
   :source-paths ["src"]
   :cljsbuild {:builds
               {:dev
